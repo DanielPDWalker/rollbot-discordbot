1a1b6d6f48e7a33e72b6eb4c5b0f82d221965b4d
danielpdwalker <danielpdwalker@gmail.com>
2020-02-01 14:11:50 +0000

    Added random rolling function to bot, using regex

--- bot.py
+++ bot.py
@@ -1,15 +1,39 @@
 # bot.py
 import discord
 import local_settings
+import random as r
+import re
 
-token = local_settings.DISCORD_TOKEN
-guild = local_settings.DISCORD_GUILD
+TOKEN = local_settings.DISCORD_TOKEN
+GUILD = local_settings.DISCORD_GUILD
 
 client = discord.Client()
 
 
 @client.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    for guild in client.guilds:
+        if guild.name == GUILD:
+            break
 
-client.run(token)
+    print(
+        f'{client.user} is connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})'
+    )
+
+
+@client.event
+async def on_message(message):
+    if message.content.startswith('!roll'):
+
+        holder = re.findall(r'\d+', message.content)
+
+        try:
+            number = int(holder[0])
+        except ValueError:
+            await message.channel.send("Please use a numberino frienderino.")
+
+        await message.channel.send(f'You have rolled a {r.randint(0,number)} on a d{number}')
+
+
+client.run(TOKEN)
